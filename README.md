423fb4150355e4e7e67c939e5c306dae3ff03b83
Retinpkumar <retinpkumar@gmail.com>
2022-02-21 09:58:14 +0530

    UPDATE app version 2

---
+++ README.md
@@ -0,0 +1,51 @@
+# 🌱 Agricultural N<sub>2</sub>O flux Predictor App
+
+![](https://github.com/Retinpkumar/Agricultural-N2O-Predictor-App/blob/main/images/app_recording.gif)  
+[Visit the app](https://agricultural-n2o.herokuapp.com/)
+
+The app predicts the amount of Agricultural Nitrous Oxide flux released into the atmosphere from intensively managed cropping systems.
+
+---
+
+
+## Motivation
+Nitrous Oxide is one of the potent greenhouse gas that is accumulating in the atmosphere at extraordinary rates. This can be largely attributed to intensification of agricultural cropping systems where the cultivated soil contribute almost 60% of the agricultural flux.
+
+Though there were previous attempts at identifying and quantifying the agricultural Nitrous Oxide flux, these attempts could only predict the results with an R2-score ranging from 25-40%. The model used in this app is aimed at outperforming the previous models and has been able to succesfully produce the results with almost 78% R2-score.
+
+
+
+## Model Used
+The app uses a machine learning model known as "Random Forest Regressor" for making the prediction. Random Forest belongs to tree based ensemble models category and hence it is tough to interpret the final output of the model. 
+Hence, I have used an explainable AI tool known as "SHAP" for understanding the model and interpreting the output.
+
+
+
+## Challenges faced
+* The data that quantified Nitrous Oxide flux was heavily right skewed and also contained few negative values.  
+* The data for Water frontage pore space, Ammonia and Nitrate had some values missing.  
+* Vegetation data was highly imbalanced with almost 84% of the data were recorded for Corn. Data for Wheat and Soy constituted only about 8.5 and 7.5% each.
+
+
+
+## Building the app
+The app was built using Python3.8 and with the help of Streamlit library.  
+Other major libraries used include Numpy, Pandas, Matplotlib, Scikit-learn and Shap.  
+The app is currently deployed on heroku.  
+
+
+## High level overview
+![](https://github.com/Retinpkumar/Agricultural-N2O-Predictor-App/blob/main/images/app_working.png)
+
+## Running the app locally
+* Inside a new directory clone the files from the repo using  
+<code> git clone https://github.com/Retinpkumar/Agricultural-N2O-Predictor-App.git</code>  
+* Create a new virtual environment and activate it.
+* Open the terminal and install the libraries and dependancies using  
+<code> pip install -r requirements.txt </code>  
+* Start and run the app by entering the code given below in the terminal  
+<code> streamlit run app.py </code>
+
+<h1 align='center'><b>.&emsp;&ensp;.&emsp;&ensp;.</b></h1>
+
+For further discussions and queries, contact me at: retinpkumar@gmail.com
--- app.py
+++ app.py
@@ -1,110 +1,181 @@
 import streamlit as st
-from src.model import predict
-from src.transformer import input_transformer
 import numpy as np
 import pandas as pd
+import shap
+import os
+import joblib
+import matplotlib.pyplot as plt
+from PIL import Image
+
 # page settings
 st.set_page_config(page_title="Patient Survival Prediction App",
-                   page_icon="🏥",
-                   layout="centered")
-# page header
-st.title(f"Patient Survival Prediction App")
+                page_icon="❤",
+                layout="wide")
 
+curr_path = os.path.dirname(os.path.realpath(__file__))
+
+st.markdown("<h1 align='center' class='app_title'><style> .app_title {color: red; word-spacing: 10px; font-size: 70px; margin-top: -100px;}  </style> Patient Survival Prediction App</h1>", unsafe_allow_html=True)
+
+st.image('saved_files/images/title_image.jpg')
+
+st.sidebar.image('saved_files/images/sidebar_image.jpg')
+
+st.markdown("<h2 align='center' class='app_question'><style> .app_question {color: black; word-spacing: 10px; font-size: 50px;} </style>Will the patient survive❓</h2>", unsafe_allow_html=True)
 
 with st.form("Prediction_form"):
-    # form header
-    st.header("Enter the input factors:")
+    st.subheader("Input the data for prediction:")
     # input elements
-    apache_3j_bodysystem = st.selectbox("apache_3j_bodysystem: ",
-                                        ["Cardiovascular", "Neurological", "Sepsis", "Respiratory", "Gastrointestinal",
-                                         "Metabolic", "Trauma", "Genitourinary", "Musculoskeletal/Skin", "Hematological",
-                                         "Gynecological"])
-    apache_4a_hospital_death_prob = st.number_input("apache_4a_hospital_death_prob: ")
-    gcs_verbal_apache = st.number_input("gcs_verbal_apache: ")
-    apache_2_bodysystem = st.selectbox("apache_2_bodysystem: " ,["Cardiovascular", "Neurologic","Respiratory", "Gastrointestinal","Metabolic", "Trauma","Undefined diagnoses","Renal/Genitourinary", "Haematologic","Undefined Diagnoses"])
-    ventilated_apache = st.selectbox("ventilated_apache: ",[0,1])
-
+    ventilated_apache = st.slider("Ventilated Apache", 0, 1)
+    with st.expander("View details for Ventilated Apache"):
+        st.write("Ventilated Apache indicates whether the patient was invasively ventilated at the time of the highest scoring arterial blood gas using the oxygenation scoring algorithm, including any mode of positive pressure ventilation delivered through a circuit attached to an endo-tracheal tube or tracheostomy.")
+    gcs_verbal_apache = st.slider("GCS Verbal Apache", 1, 5, step=1)
+    with st.expander("View details for GCS Verbal Apache"):
+        st.write("The verbal component of the Glasgow Coma Scale measured during the first 24 hours which results in the highest APACHE III score.")
+    gcs_eyes_apache = st.slider("GCS Eyes Apache", 1, 4, step=1)
+    with st.expander("View details for GCS Eyes Apache"):
+        st.write("The eye opening component of the Glasgow Coma Scale measured during the first 24 hours which results in the highest APACHE III score")
+    gcs_motor_apache = st.slider("gcs_motor_apache", 1, 6, step=1)
+    with st.expander("View details for GCS Motor Apache"):
+        st.write("The motor component of the Glasgow Coma Scale measured during the first 24 hours which results in the highest APACHE III score")
+    intubated_apache = st.slider("Intubated Apache", 0, 1)
+    with st.expander("View details for Intubated Apache"):
+        st.write("Whether the patient was intubated at the time of the highest scoring arterial blood gas used in the oxygenation score")
+    apache_2_bodysystem = st.selectbox("Apache 2 Bodysystem", ['Cardiovascular', 'Respiratory', 'Metabolic', 'Trauma', 'Neurologic', 'Gastrointestinal', 'Renal/Genitourinary', 'Undefined', 'Haematologic'])
+    with st.expander("View details for Apache 2 Bodysystem"):
+        st.write("Admission diagnosis group for APACHE II")
+    apache_3j_bodysystem = st.selectbox("Apache 3J Bodysystem", ['Sepsis', 'Respiratory', 'Metabolic', 'Cardiovascular', 'Trauma', 'Neurological', 'Gastrointestinal', 'Genitourinary', 'Haematological', 'Musculoskeletal/Skin', 'Gynecological'])
+    with st.expander("View details for Apache 3J Bodysystem"):
+        st.write("Admission diagnosis group for APACHE III")
+    apache_2_diagnosis = st.number_input("Apache 2 Diagnosis")
+    with st.expander("View details for Apache 2 Diagnosis"):
+        st.write("The APACHE II diagnosis for the ICU admission")
+    age = st.number_input("Age")
+    explain_model = st.checkbox("Show model explanation using Explainable AI")
     # submitting values
     submit_val = st.form_submit_button("Predict")
-
+    
+st.sidebar.markdown("<h2 class='sidehead' align='center'><style> .sidehead {color:red; background:white; border-radius: 5px;}</style> Patient Survival Prediction App</h2>", unsafe_allow_html=True)
+st.sidebar.markdown("<h2>About the app</h2>", unsafe_allow_html=True)
+st.sidebar.write("The app predicts whether a person admitted to a hospital will survive or not.")
+    
 if submit_val:
-    # for apache_3j_bodysystem
-    if apache_3j_bodysystem == "Cardiovascular":
-        apache_3j_bodysystem = 1
-    elif apache_3j_bodysystem == "Neurological":
-        apache_3j_bodysystem = 2
-    elif apache_3j_bodysystem == "Sepsis":
-        apache_3j_bodysystem = 3
-    elif apache_3j_bodysystem == "Respiratory":
-        apache_3j_bodysystem = 4
-    elif apache_3j_bodysystem == "Gastrointestinal":
-        apache_3j_bodysystem = 5
-    elif apache_3j_bodysystem == "Metabolic":
-        apache_3j_bodysystem = 6
-    elif apache_3j_bodysystem == "Trauma":
-        apache_3j_bodysystem = 7
-    elif apache_3j_bodysystem == "Genitourinary":
-        apache_3j_bodysystem = 8
-    elif apache_3j_bodysystem == "Musculoskeletal/Skin":
-        apache_3j_bodysystem = 9
-    elif apache_3j_bodysystem == "Hematological":
-        apache_3j_bodysystem = 10
-    else:
-        apache_3j_bodysystem = 11
+    # input transformation
+    apache_2_bodysystem_dict = {
+        'Cardiovascular': 0.4232,
+        'Respiratory': 0.1265,
+        'Metabolic': 0.0834,
+        'Trauma': 0.0418,
+        'Neurologic': 0.1297,
+        'Gastrointestinal': 0.0984,
+        'Renal/Genitourinary': 0.0268,
+        'Haematologic': 0.0069,
+        'Undefined':  0.0448
+    }
+    for k,v in apache_2_bodysystem_dict.items():
+        if apache_2_bodysystem == k:
+            apache_2_bodysystem = v
 
-    # for apache_2_bodysystem
-    if apache_2_bodysystem == "Cardiovascular":
-        apache_2_bodysystem = 1
-    elif apache_2_bodysystem == "Neurological":
-        apache_2_bodysystem = 2
-    elif apache_2_bodysystem == "Respiratory":
-        apache_2_bodysystem = 3
-    elif apache_2_bodysystem == "Gastrointestinal":
-        apache_2_bodysystem = 4
-    elif apache_2_bodysystem == "Metabolic":
-        apache_2_bodysystem = 5
-    elif apache_2_bodysystem == "Trauma":
-        apache_2_bodysystem = 6
-    elif apache_2_bodysystem == "Undefined diagnoses":
-        apache_2_bodysystem = 7
-    elif apache_2_bodysystem == "Renal/Genitourinary":
-        apache_2_bodysystem = 8
-    elif apache_2_bodysystem == "Hematological":
-        apache_2_bodysystem = 9
-    elif apache_2_bodysystem == "Undefined Diagnoses":
-        apache_2_bodysystem = 10
-    else:
-        apache_2_bodysystem = 11
+    apache_3j_bodysystem_dict = {
+        'Sepsis': 0.1280,
+        'Respiratory': 0.1265,
+        'Metabolic': 0.0834,
+        'Cardiovascular': 0.3270,
+        'Trauma': 0.0418,
+        'Neurological': 0.1297,
+        'Gastrointestinal': 0.0984,
+        'Genitourinary': 0.0236,
+        'Haematological': 0.0069,
+        'Musculoskeletal/Skin': 0.0127,
+        'Gynecological':  0.0034
+    }
+    for k,v in apache_3j_bodysystem_dict.items():
+        if apache_3j_bodysystem == k:
+            apache_3j_bodysystem = v
 
-    # for apache_4a_hospital_death_prob
-    if apache_4a_hospital_death_prob < 0.05:
-        apache_4a_hospital_death_prob = 0
-    else:
-        apache_4a_hospital_death_prob = 1
+    # creating apache_bodysystem
+    apache_bodysystem = 2 * apache_2_bodysystem * apache_3j_bodysystem/(apache_2_bodysystem + apache_3j_bodysystem + 0.00001)
 
     # list of features
-    feats = ['apache_3j_bodysystem', 'apache_4a_hospital_death_prob', 'gcs_verbal_apache', 'apache_2_bodysystem', 'ventilated_apache']
+    feats = [
+        'ventilated_apache',
+        'gcs_motor_apache',
+        'gcs_eyes_apache',
+        'gcs_verbal_apache', 
+        'intubated_apache',
+        'apache_bodysystem', 
+        'apache_2_diagnosis',
+        'age'
+        ]
+    # saving feature names
+    joblib.dump(feats, 'src/models/features.sav')
     # list of corresponding input values
-    attribute_vals = [apache_3j_bodysystem, apache_4a_hospital_death_prob, gcs_verbal_apache, apache_2_bodysystem, ventilated_apache]
+    attribute_vals = [ventilated_apache, gcs_motor_apache, gcs_eyes_apache, gcs_verbal_apache, intubated_apache, apache_bodysystem, apache_2_diagnosis, age]
+
     # dictionary of features and values
     attr_dict = dict(zip(feats, attribute_vals))
     # dataframe for scaling and model input
     attr_df = pd.DataFrame(attr_dict, index=[1])
-    # getting values from training data to transform incoming inputs
-    train_df = input_transformer()
-    # log transform
-    attr_df['gcs_verbal_apache'] = np.log1p(attr_df['gcs_verbal_apache'])
-    # capping upper and lower values
-    attr_df['gcs_verbal_apache'] = np.where(attr_df['gcs_verbal_apache']>max(train_df['gcs_verbal_apache']), max(train_df['gcs_verbal_apache']),
-                        np.where(attr_df['gcs_verbal_apache']<min(train_df['gcs_verbal_apache']), min(train_df['gcs_verbal_apache']), attr_df['gcs_verbal_apache']))
+
+    # winsorizing features
+    caps = joblib.load('src/utils/winsorizer.sav')
+    for item in caps:
+        for feat in ['apache_2_diagnosis', 'age']:
+            if feat==item[0]:
+                attr_df[feat] = np.where(attr_df[feat]<item[1], item[1], np.where(attr_df[feat]>item[2], item[2], attr_df[feat])) 
+
+    # scaling input data
+    scaler = joblib.load('src/utils/scaler_selected.sav')
+    scaled_df = scaler.transform(attr_df)
+
+    # load the model
+    model = joblib.load('src/utils/final_model.sav')
 
     # predicted value from the model
-    value = predict(attributes=attr_df)
-    # results header
-    st.header("Result: ")
+    value = model.predict(scaled_df)
+
     # output results
-    if value:
-        st.error(f"The patient died during hospitalization")
+    if value!=0:
+        st.markdown("<h1 align='center' class='fail'> <style> .fail {color: red;} </style>Unfortunately, the patient will not survive.</h1>", unsafe_allow_html=True)
     else:
-        st.success(f"The patient survived during hospitalization")
-        st.balloons()
+        st.markdown("<h1 align='center' class='success'> <style> .success {color: green;}</style>The patient will survive.</h1>", unsafe_allow_html=True)
+
+    input_attributes = np.array(attribute_vals)
+    force_plot_path = '/saved_files/plots/force_plot.png'
+
+    shap_explainer = shap.TreeExplainer(model)
+    shap_model_values = shap_explainer.shap_values(input_attributes)
+    shap_model_expected_values = shap_explainer.expected_value
+
+    shap.force_plot(shap_model_expected_values[1], 
+                    shap_model_values[1], 
+                    input_attributes,
+                    feats,
+                    show=False,
+                    matplotlib=True).savefig(curr_path + force_plot_path, bbox_inches='tight')
+    
+    force_plot_image = Image.open(curr_path + force_plot_path)
+
+if submit_val and explain_model:
+    st.markdown('---')
+    st.markdown("<h2 align='center'>Marginal contribution of input features in the prediction</h2>", unsafe_allow_html=True)
+    st.image(force_plot_image)
+
+    st.markdown("<h2 align='center'>Contribution of features in the model</h2>", unsafe_allow_html=True)
+    st.markdown("""<p>
+    This plot displays the summary of the impact of top features in a dataset on the model prediction. 
+    Each dot represents each individual observation in the dataset. 
+    <p style="color:blue;"> Blue dots indicate those observations that impact the prediction that the patient would survive.</p>
+    <p style="color:red;"> Red dots indicate those observations that impact the prediction that the patient would not survive.</p> 
+    The features are arranged in the decreasing order of priority and hence the topmost feature is
+    the most important one.</p>
+    """, unsafe_allow_html=True)
+
+    st.image("saved_files/plots/feature_contribution.png")
+
+    st.markdown("""<p>We find that <b>'ventilated_apache'</b> is the feature with the most impact on the model output and <b>'intubated_apache'</b> is the feature with the
+    least impact on the model output.</p>
+    <p style="color:red;">Higher values of <b>'ventialted_apache'</b> and <b>'age'</b> has a greater impact on the prediction that the patient would not survive. </p>
+    <p style="color:green;">Whereas higher values for <b>'gcs_motor_apache'</b>, <b>'gcs_verbal_apache'</b> and <b>'gcs_eyes_apache'</b>
+    has a greater impact on the prediction that the patient would survive.</p>""", unsafe_allow_html=True)
+st.sidebar.markdown('---')
+st.sidebar.markdown("<h3 align='center'><a href='https://github.com/Retinpkumar'>Made by: Retin P Kumar</h3></a>", unsafe_allow_html=True)
